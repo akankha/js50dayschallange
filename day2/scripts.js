9212aa733027b203b1733386a95c06ae5f336785
Md. Mohi Uddin Ahmed <mohiuddin.ahmed@thecitybank.com>
2023-01-08 23:11:43 +0600

    day 2 | js added

--- day2/scripts.js
+++ day2/scripts.js
@@ -1,16 +1,35 @@
-const panels = document.querySelectorAll('.panel')
+const progress = document.getElementById('progress')
+const prev = document.getElementById('prev')
+const next = document.getElementById('next')
+const circles = document.querySelectorAll('.circle')
 
+let currentActive = 1
 
-panels.forEach((panel)=>{
-    panel.addEventListener('click',()=>{
-        removeActiveClasses()
-        panel.classList.add('active')
-        
-    })
+next.addEventListener('click',()=>{
+    currentActive ++
+    if(currentActive > circles.length){
+        currentActive = circles.length
+    } 
+    update();
 })
 
-function removeActiveClasses(){
-    panels.forEach((panel)=>{
-        panel.classList.remove('active')
+prev.addEventListener('click',()=>{
+    currentActive --
+    if(currentActive < circles.length){
+        currentActive = circles.length
+    }
+    update();
+    
+})
+function update(){
+    circles.forEach((circle,idx)=>{
+        if(idx < currentActive){ 
+            circle.classList.add('active')
+        }else{
+            circle.classList.remove('active')
+        }      
     })
 }
+
+const actives = document.querySelectorAll('.active')
+console.log(actives.length / circles.length)
